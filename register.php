d0cac973a89a58b39de173adf51ed1651c0e2f2d
Andrea <andreacb94>
2017-09-13 17:11:07 +0200

    Initial commit

---
+++ register.php
@@ -0,0 +1,32 @@
+<?php
+
+    $username = $_POST["username"];
+    $password = $_POST["password"];
+    $error = "";
+
+    $username = filter_var($username, FILTER_SANITIZE_STRING);
+    $password = filter_var($password, FILTER_SANITIZE_STRING);
+
+    if(!$username || !$password)
+        $error = "Username e password sono obbligatori!";
+
+?>
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <title></title>
+    </head>
+    <body>
+        <h1> Risultati registrazione</h1>
+
+        <?php if($error): ?>
+            <p style="color: red;"> <?php echo $error ?> </p>
+
+        <?php else: ?>
+            <p>Benvenuto <?php echo $username ?> </p>
+
+        <?php endif ?>
+
+    </body>
+</html>
